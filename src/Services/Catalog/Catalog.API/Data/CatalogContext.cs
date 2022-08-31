10236b6d194744486d22a41b274d126c2f4219d5
Rafael Camara <rafaelcamarac@gmail.com>
2022-08-31 22:37:50 +0100

    Add remaining files.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var mongoClient = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            /*
+                Retrieves the specified database.
+                If such database does not exists, it will create one
+             */
+            var catalogDatabase = mongoClient
+                                            .GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = catalogDatabase.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+    }
+}
+
