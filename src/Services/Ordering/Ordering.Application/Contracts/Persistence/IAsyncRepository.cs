27a3a6198628ca28ba8bb6ddae1549a6bbcb418b
Rafael Camara <rafaelcamarac@gmail.com>
2022-09-25 12:18:01 +0100

    Add Ordering Infrastructure layer.

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -12,6 +12,7 @@ namespace Ordering.Application.Contracts.Persistence
 	{
 		Task<IEnumerable<T>> GetAllAsync();
 		Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate);
+		/*
 		Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
 										Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
 										string includeString = null,
@@ -20,6 +21,7 @@ namespace Ordering.Application.Contracts.Persistence
 									   Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
 									   List<Expression<Func<T, object>>> includes = null,
 									   bool disableTracking = true);
+		*/
 		Task<T> GetByIdAsync(int id);
 		Task<T> AddAsync(T entity);
 		Task UpdateAsync(T entity);
